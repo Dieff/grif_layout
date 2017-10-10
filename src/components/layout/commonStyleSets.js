744d30137a8593f63f65164b26d25f86460048da
Dieff <nick.dieff@gmail.com>
2017-10-09 20:49:42 -0400

    Reorganized code

--- src/components/layout/commonStyleSets.js
+++ src/components/layout/commonStyleSets.js
@@ -1,50 +1,7 @@
 import PropTypes from 'prop-types';
+import styleCreatorFactory from './styleSetFactory';
 
 //window.matchMedia
-
-const styleCreatorFactory = (preStylePropertiesMap) => {
-  const stylePropertiesMap = new Map(
-    [...preStylePropertiesMap.keys()].map(
-      propName => [
-        propName,
-        ((propDef) => {
-          const newPropDef = {};
-          if (!('name' in propDef)) {
-            console.warn('no prop name');
-          } else {
-            newPropDef.name = propDef.name;
-          }
-          newPropDef.validator = ('validator' in propDef) ? 
-            propDef.validator : (f, g) => f;
-          newPropDef.type = ('type' in propDef) ? 
-            propDef.type : PropTypes.oneOfType([PropTypes.string, PropTypes.number]);
-          
-          return newPropDef;
-        })(preStylePropertiesMap.get(propName))
-      ]
-    )
-  );
-
-  const creator = (componentProps) => {
-    const styles = {};
-    Object.keys(componentProps).filter(
-      key => stylePropertiesMap.has(key)
-    ).forEach(
-      key => {
-        const styleProp = stylePropertiesMap.get(key);
-        styles[styleProp.name] = styleProp.validator(componentProps[key], componentProps);
-      }
-    );
-    return styles;
-  };
-
-  creator.propTypes = {};
-  [...stylePropertiesMap.keys()].forEach(
-    key => { creator.propTypes[key] = stylePropertiesMap.get(key).type; }
-  );
-
-  return creator;
-};
 
 const padding = styleCreatorFactory(
   new Map([
---
+++ src/components/layout/styleSetFactory.js
@@ -0,0 +1,45 @@
+const styleCreatorFactory = (preStylePropertiesMap) => {
+  const stylePropertiesMap = new Map(
+    [...preStylePropertiesMap.keys()].map(
+      propName => [
+        propName,
+        ((propDef) => {
+          const newPropDef = {};
+          if (!('name' in propDef)) {
+            console.warn('no prop name');
+          } else {
+            newPropDef.name = propDef.name;
+          }
+          newPropDef.validator = ('validator' in propDef) ? 
+            propDef.validator : (f, g) => f;
+          newPropDef.type = ('type' in propDef) ? 
+            propDef.type : PropTypes.oneOfType([PropTypes.string, PropTypes.number]);
+          
+          return newPropDef;
+        })(preStylePropertiesMap.get(propName))
+      ]
+    )
+  );
+
+  const creator = (componentProps) => {
+    const styles = {};
+    Object.keys(componentProps).filter(
+      key => stylePropertiesMap.has(key)
+    ).forEach(
+      key => {
+        const styleProp = stylePropertiesMap.get(key);
+        styles[styleProp.name] = styleProp.validator(componentProps[key], componentProps);
+      }
+    );
+    return styles;
+  };
+
+  creator.propTypes = {};
+  [...stylePropertiesMap.keys()].forEach(
+    key => { creator.propTypes[key] = stylePropertiesMap.get(key).type; }
+  );
+
+  return creator;
+};
+
+export default styleCreatorFactory;
